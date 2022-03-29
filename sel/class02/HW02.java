43e2a5a24637137ebfa2f1ed3a76627a6c0ae2c1
mmm2k-bit <mmm2k@pm.me>
2022-03-28 20:42:44 -0500

    try2

--- sel/class02/HW02.java
+++ sel/class02/HW02.java
@@ -1,2 +1,12 @@
-package com.sel.class02;public class HW02 {
+package com.sel.class02;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HW02 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        ChromeDriver chrome = new ChromeDriver();
+        chrome.get("https://www.facebook.com/");
+
+    }
 }
--- sel/class03/XpathTest.java
+++ sel/class03/XpathTest.java
@@ -1,2 +1,23 @@
-package com.sel.class03;public class XpathTest {
+package com.sel.class03;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+       WebElement username= driver.findElement(By.xpath("//input[contains(@id, 'username')]"));
+       username.sendKeys("Tester");
+       WebElement password = driver.findElement( By.xpath("//input[@type= 'password']"));
+       password.sendKeys("test");
+       WebElement login = driver.findElement(By.xpath("//input[@value= 'Login']"));
+       login.click();
+       driver.quit();
+
+
+    }
 }
